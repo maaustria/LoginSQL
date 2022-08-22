6d327043d80f96671e68844cf3b50ce1b9c5cf47
maaustria <maaustria16@gmail.com>
2022-08-22 23:50:31 +0800

    Initial commit

---
+++ app/src/main/java/com/android/sqldemo/MainActivity.java
@@ -0,0 +1,72 @@
+package com.android.sqldemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username,password,repassword;
+    Button signup,signin;
+    DBHelper DB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        signup = findViewById(R.id.signup);
+        signin = findViewById(R.id.signin);
+        DB = new DBHelper(this);
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if(TextUtils.isEmpty(user) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(repass))
+                    Toast.makeText(MainActivity.this, "All Fields Required", Toast.LENGTH_SHORT).show();
+                else{
+                    if (pass.equals(repass)) {
+                        Boolean checkuser = DB.checkusername(user);
+                        if (checkuser==false){
+                            Boolean insert = DB.insertData(user,pass);
+                            if(insert==true){
+                                Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(MainActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }   
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Username already Exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Password do not Match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
